969f63669d94c8f339239fcaaeda624c8a3c5945
Jagannath kashyap <jagannathkashyap38@gmail.com>
2025-06-13 06:03:50 +0530

    tweetpost component

---
+++ frontend/src/component/tweetcomponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TweetComponent = () => {
+    return (
+        <div className="tweetpost">
+            <img src="https://pbs.twimg.com/profile_images/1933046314012717059/hsrzsRpB_400x400.jpg" alt="" srcset="" />
+            <div className="username">
+                <h3>Arun</h3>
+                <h3>@hiarun01</h3>
+                <h3>. 7h</h3>
+
+            </div>
+
+        </div>
+    )
+}
